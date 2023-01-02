670ada3ca098fcc8e1b6fe4206a89cd1e64f0597
oluwajueda <oluwajuedaloade@gmail.com>
2023-01-02 20:02:18 +0100

    changes made to Getnotification file

--- lib/screens/GetNotification.dart
+++ lib/screens/GetNotification.dart
@@ -1,8 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:getfitts/screens/Information.dart';
 import 'package:getfitts/screens/Login.dart';
 
