3bd5dd9a5039a1389a2919a758511b7cfc635653
oluwajueda <oluwajuedaloade@gmail.com>
2023-01-04 15:30:14 +0100

    remove unused imports

--- lib/screens/Vitals.dart
+++ lib/screens/Vitals.dart
@@ -4,7 +4,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:getfitts/model/vitals.dart';
 import 'package:intl/intl.dart';
-import 'package:provider/provider.dart';
 
 class Vitals extends StatefulWidget {
   const Vitals({super.key});
