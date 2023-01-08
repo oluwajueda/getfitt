d6c3d4bea2702a6e48b11b7b414269663cd78956
oluwajueda <oluwajuedaloade@gmail.com>
2023-01-08 22:34:38 +0100

    remove other unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:getfitts/screens/ExerciseScreen.dart';
 import 'package:getfitts/screens/Home.dart';
 import 'package:getfitts/screens/LandingPage.dart';
-import 'package:getfitts/screens/Login.dart';
 import 'package:getfitts/screens/VerifyPhone.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:getfitts/screens/Vitals.dart';
 import 'package:getfitts/screens/VitalsPage.dart';
 import 'package:getfitts/screens/resting.dart';
 import 'package:getfitts/utils/application_state.dart';
